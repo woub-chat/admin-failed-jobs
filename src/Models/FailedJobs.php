6681f6a753230a6e01130d9d3dcc3dc14cc4ac26
xsaven <gms.sw.xsaven@gmail.com>
2023-11-08 23:48:26 +0200

    composer.json, src/Models/FailedJobs.php

--- src/Models/FailedJobs.php
+++ src/Models/FailedJobs.php
@@ -5,6 +5,8 @@ namespace Admin\Extend\AdminFailedJobs\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * Admin\Extend\AdminFailedJobs\Models\FailedJobs
+ *
  * @property int $id
  * @property string $connection
  * @property string $queue
@@ -20,6 +22,9 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|FailedJobs whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|FailedJobs wherePayload($value)
  * @method static \Illuminate\Database\Eloquent\Builder|FailedJobs whereQueue($value)
+ * @property string $uuid
+ * @method static \Illuminate\Database\Eloquent\Builder|FailedJobs whereUuid($value)
+ * @mixin \Eloquent
  */
 class FailedJobs extends Model
 {
